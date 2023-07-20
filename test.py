dcdcd581c4b522d7db479a1ffbf64bd260d0d283
nasreddine hamdi <hamdi.nasreddine@yahoo.fr>
2023-07-20 14:15:11 +0100

    ajout inner class date

---
+++ test.py
@@ -0,0 +1,10 @@
+class person:
+  def __init__(self, name,prenom):
+     self.name=name
+     self.prenom=prenom
+  
+  class date:
+    def __init_(self,jours,mois,annee):
+       self.jours=jours
+       self.mois=mois
+       self.annee=anee
